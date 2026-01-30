c4bd6e6ebacf511e1c5020064462e4755d92cab6
Jakenology <37952082+jakenology@users.noreply.github.com>
2026-01-30 08:49:19 -0600

    Create index.mjs for PetLibroAPI exports

---
+++ index.mjs
@@ -0,0 +1,7 @@
+import PetLibroAPI from './index.js';
+
+// Default export (allows: import PetLibroAPI from 'petlibro-api')
+export default PetLibroAPI;
+
+// Named export (allows: import { PetLibroAPI } from 'petlibro-api')
+export { PetLibroAPI };
